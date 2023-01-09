d15c7c534a920bb4f0a5b4339ff5eb64bc6efe02
Tembulat Tokmakov <tembatokmakov43@gmail.com>
2023-01-09 19:27:21 +0300

    Second homework

--- src/Buffered.java
+++ src/Buffered.java
@@ -1,18 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-        int eaters = 5; // сколько людей будут есть
+public class Buffered {
+    public static void main(String[] args)
+            throws IOException {
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+        String ticketPrc = reader.readLine();
 
-        int water = 3000; // миллилитров воды
-        int potatoes = 5; // картофелин
-        int chicken = 6; // куриных бёдер
-        int spices = 10; // ложек специй
+        int ticket = Integer.parseInt(ticketPrc);
+        int miles = ticket / 20;
 
-        System.out.println("Сварили суп. На одного человека вышло:");
-        System.out.println((water / eaters) + " миллилитров воды");
-        System.out.println((potatoes / eaters) + " картофелин(а)");
-        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
-        System.out.println((spices / eaters) + " ложек(ка) специй");
+        System.out.println("Кол-во милей - " + miles);
 
     }
 }
